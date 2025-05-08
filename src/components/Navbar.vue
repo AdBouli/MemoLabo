9cf028147f458c7b069c82958ed6fd113d0079d9
Adrien Boulineau <adbouli@vivaldi.net>
2025-05-08 16:26:46 +0200

    fix navbar responsive

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,36 +1,30 @@
 <template>
-    <header class="navbar navbar-expand-lg bg-body-secondary">
+    <header class="navbar navbar-expand-lg bg-body-tertiary">
         <nav class="container">
-            <div class="navbar-nav">
-                <a class="navbar-brand" href="#">{{ $appName }}</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#nav-menu">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="nav-menu">
-                    <ul class="navbar-nav">
-                        <li class="nav-item">
-                            <a class="nav-link active" href="#">Menu 1</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Menu 2</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Menu3</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Menu 4</a>
-                        </li>
-                    </ul>
-                </div>
+            <a class="navbar-brand" href="#">{{ $appName }}</a>
+            <button class="navbar-toggler" type="button"
+                data-bs-toggle="collapse" data-bs-target="#nav-menu">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="nav-menu">
+                <ul class="navbar-nav">
+                    <a class="nav-link active" href="#">Menu 1</a>
+                    <a class="nav-link" href="#">Menu 2</a>
+                    <a class="nav-link" href="#">Menu3</a>
+                    <a class="nav-link" href="#">Menu 4</a>
+                    <div class="form-check form-switch d-lg-none" >
+                        <input type="checkbox" class="form-check-input" id="lightDarkSwitch"
+                            :checked="isDarkMode" @change="toggleDarkMode"/>
+                        <label class="form-check-label" for="lightDarkSwitch" id="lightDarkLabel">
+                            <i v-if="isDarkMode" class="bi bi-moon-stars-fill"></i>
+                            <i v-else class="bi bi-sun-fill"></i>
+                        </label>
+                    </div>
+                </ul>
             </div>
-            <div class="form-check form-switch" >
-                <input
-                    class="form-check-input"
-                    type="checkbox"
-                    id="lightDarkSwitch"
-                    :checked="isDarkMode"
-                    @change="toggleDarkMode"
-                />
+            <div class="form-check form-switch d-none d-lg-block">
+                <input type="checkbox" class="form-check-input" id="lightDarkSwitch"
+                    :checked="isDarkMode" @change="toggleDarkMode" />
                 <label class="form-check-label" for="lightDarkSwitch" id="lightDarkLabel">
                     <i v-if="isDarkMode" class="bi bi-moon-stars-fill"></i>
                     <i v-else class="bi bi-sun-fill"></i>
