feca1705487f50b9d68ed54388bcf7971cb6bf16
Adrien Boulineau <adbouli@vivaldi.net>
2025-05-11 01:17:54 +0200

    suppression du doublon d'import de bootstrap

--- src/main.ts
+++ src/main.ts
@@ -3,16 +3,15 @@
 // Description: Fichier principal de l'application Vue.js
 
 // Importation des styles et des dépendances
-import './assets/bootstrap-custom.scss'
-import 'bootstrap'
+import '@/assets/bootstrap-custom.scss'
 import 'bootstrap-icons/font/bootstrap-icons.css'
-import { router } from '@/router'
 
 // Importation de Vue et de l'application principale
 import { createApp } from 'vue'
 import App from '@/App.vue'
-import vBootstrap from '@/directives/Bootstrap'
 import { setGlobalProperties } from '@/start/GlobalProperties'
+import vBootstrap from '@/directives/Bootstrap'
+import { router } from '@/router'
 
 // Création de l'application 
 const app = createApp(App)
