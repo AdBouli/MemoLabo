2b0e5eb46c04e7ca9bb6e80d2c1e78ca4ad43f9b
Adrien Boulineau <adbouli@vivaldi.net>
2025-05-11 18:15:21 +0200

    les formulaires des fonctions math respectent le theme de couleur utilisateur

--- src/components/modules/MathFunction/Forms/MathFuncForm.vue
+++ src/components/modules/MathFunction/Forms/MathFuncForm.vue
@@ -1,23 +1,23 @@
 <template>
     <div class="input-group mb-2">
         <!-- Id span -->
-        <span class="input-group-text text-dark border border-primary rounded-start ps-2 pe-1"
-            :style="{backgroundColor: `${func.color}80` }">
+        <span class="input-group-text border border-primary rounded-start ps-2 pe-1"
+            :style="{backgroundColor: bgColor }">
             <strong><em>f(x)</em></strong>
         </span>
         <!-- Input expression -->
         <input type="text" class="form-control border border-primary w-50" :class="{ 'is-invalid': func.error }"
             v-model="func.expression" @keyup="func.compile()" />
         <!-- Input couleur -->
-        <button class="btn btn-outline-primary text-dark"
-            @click="openColorPicker"  :style="{backgroundColor: `${func.color}80` }">
+        <button class="btn btn-outline-primary text-reset"
+            @click="openColorPicker"  :style="{backgroundColor: bgColor }">
             <i class="bi bi-palette-fill"></i>
             <input type="color" style="opacity: 0; position: absolute; pointer-events: none; width: 0; height: 0;"
                 ref="colorPicker" v-model="func.color">
         </button>
         <!-- Bouton suppression -->
-        <button class="btn btn-outline-primary text-dark"
-            @click="$emit('delete', func)"  :style="{backgroundColor: `${func.color}80` }">
+        <button class="btn btn-outline-primary text-reset"
+            @click="$emit('delete', func)"  :style="{backgroundColor: bgColor }">
             <i class="bi bi-trash-fill"></i>
         </button>
     </div>
@@ -25,10 +25,11 @@
 
 <script setup lang="ts">
 
-import { ref } from 'vue'
+import { onMounted, ref, watch } from 'vue'
 import { MathFunc } from '@/components/modules/MathFunction/Models/MathFunc'
 
 const colorPicker = ref<HTMLInputElement | null>(null)
+const bgColor = ref<string>("")
 
 const func = defineModel({
     type: Object as () => MathFunc,
@@ -42,5 +43,19 @@ const openColorPicker = () => {
 }
 
 const emit = defineEmits(['delete'])
+
+const ALPHA_TRANSPARENCE = '80'
+
+const updateBgColor = () => {
+    bgColor.value = `${func.value.color}${ALPHA_TRANSPARENCE}`
+}
+
+onMounted(() => {
+    updateBgColor()
+})
+
+watch(() => func, () => {
+    updateBgColor()
+}, { deep: true })
 
 </script>
